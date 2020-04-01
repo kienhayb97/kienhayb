000d009dea4cc606ad75c6331fddbac826c65011
kienhayb97 <kienhayb.21091997@gmail.com>
2020-04-01 13:48:23 +0700

    my first commt'

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/API_BOOK_STORE'));
+app.use(express.static(__dirname + '/dist/BookStore-Angular'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/API_BOOK_STORE/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/BookStore-Angular/index.html'));
 });
 
 // default Heroku PORT
